cb5f32b63d7e77f1060673583fd42311ba0ec07a
Charlotte <csljeffreys@gmail.com>
2022-03-08 15:32:52 +0000

    Add document query selectors for all the buttons

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const sevenButton = document.querySelector("#seven-button");
+const eightButton = document.querySelector("#eight-button");
+const nineButton = document.querySelector("#nine-button");
+const fourButton = document.querySelector("#four-button");
+const fiveButton = document.querySelector("#five-button");
+const sixButton = document.querySelector("#six-button");
+const oneButton = document.querySelector("#one-button");
+const twoButton = document.querySelector("#two-button");
+const threeButton = document.querySelector("#three-button");
+const zeroButton = document.querySelector("#zero-button");
+
+const clearButton = document.querySelector("#clear-button");
+const deleteButton = document.querySelector("#delete-button");
+const decimalPointButton = document.querySelector("#decimal-point-button");
+
+const addButton = document.querySelector("#add-button");
+const subtractButton = document.querySelector("#subtract-button");
+const multiplyButton = document.querySelector("#multiply-button");
+const divideButton = document.querySelector("#divide-button");
+const equalsButton = document.querySelector("#equals-button");
+
+
+
+
+
 function add(x, y){
     return x + y;
 }
